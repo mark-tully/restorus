4c3a1833108f42be772ae15860d26d88ad3bfa27
Mark Tully <mark.tully@restorus.org>
2014-09-12 22:06:19 -0700

    more static root

--- restorus/settings.py
+++ restorus/settings.py
@@ -48,9 +48,8 @@ MEDIA_URL = os.path.join(os.path.dirname(__file__), '../media/')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = (
-    os.path.join(os.path.dirname(__file__), '../assets/'),
-)
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../assets/')
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
