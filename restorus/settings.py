593403010c4461320ae48c0be0ba15f11176efed
Mark Tully <marktully23@gmail.com>
2014-10-03 16:42:22 -0700

    redirects

--- restorus/settings.py
+++ restorus/settings.py
@@ -96,6 +96,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'restorus.urls'
